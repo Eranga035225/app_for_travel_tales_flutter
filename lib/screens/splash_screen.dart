b013e514fbf75fa8302dd8764b390591568976b1
Eranga <erangakavishanka@icloud.com>
2025-11-04 21:56:34 +0530

    widgets in the splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,9 +6,16 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 3, 93, 163),
       body: Center(
-        
-        child: Text('Travel Tales')
+
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Travel Tales', style: TextStyle(color:Colors.white, fontSize: 35, fontWeight: FontWeight.bold)),
+            Icon(Icons.public, color: Colors.white,size: 35,)
+          ],
+        )
       
       )
 
