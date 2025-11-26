acceb9566fd1277e8d97e723ab423d012f49d90e
Eranga <erangakavishanka@icloud.com>
2025-11-26 23:37:55 +0530

    developed upper row in the screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,29 +13,41 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(children: [
-          Row(children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Hi, Era👋", style:TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w500
-                  
-                        
-                )),
-                Text('Explore the world', style:TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w300
-
-            ))
-              ],
-            ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Hi, Era👋", style:TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w500
+                    
+                          
+                  )),
+                  Text('Explore the world', style:TextStyle(
+                  fontSize: 16,
+                  color:Colors.grey.shade600,
+                  fontWeight: FontWeight.w300
           
-        
-          ],)
-        
-        ],),
+              ))
+                ],
+              ),
+             
+              CircleAvatar(
+                radius: 25,
+                backgroundImage: NetworkImage('https://th.bing.com/th/id/OIP.OmJICjo6Xt-Ay8oWfxkGNQHaHa?w=183&h=182&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3'),
+              ),
+              
+            
+          
+            ],)
+          
+          ],),
+        ),
       )
 
       
