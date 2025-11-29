fff1888bd06bb7f5b9866d8d35c9fb0e5f037a74
Eranga <erangakavishanka@icloud.com>
2025-11-29 23:16:02 +0530

    extracted in to  widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -193,99 +193,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ],),
             ),
             SizedBox(height:20,),
-            Container(
-              width: size.width* 0.7,
-              height: (size.width* 0.7)*1.2,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/2.jpeg'),
-                  fit: BoxFit.cover
-                )
-
-              ),
-
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: 12,right:8,
-                    child: CircleAvatar(
-                      radius: 20,
-                      child:Icon(Icons.favorite_border, color:Colors.grey.shade200,),
-                      backgroundColor: Colors.black.withAlpha(30),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom:8.0),
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        width: size.width* 0.6,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          
-                          color: Colors.grey.shade900.withAlpha(90),
-                          borderRadius: BorderRadius.circular(15)
-                        ),
-                        child:Column(
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text('Mount Narangala', style:TextStyle(
-                                  color:Colors.white,
-                                  fontSize:16,
-                                  fontWeight: FontWeight.w800
-                                ),),
-                                Text(', Sri Lanka', style:TextStyle(
-                                  color:Colors.grey.shade100,
-                                  fontSize:13,
-                                  fontWeight: FontWeight.w400
-                                ),),  
-                              ],
-
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(Icons.location_on_rounded, color: Colors.grey.shade300,size:20),
-                                    SizedBox(width:4,),
-                                    Text('Badulla, Sri Lanka', style:TextStyle(
-                                      color:Colors.grey.shade300,
-                                      fontSize:14,
-                                      fontWeight: FontWeight.w400
-                                    ),),
-                                
-                                    
-                                
-                                  ],
-                                ),
-                                Row(children: [
-                                  Icon(Icons.star_border, color: Colors.amber.shade400,size: 20),
-                                  Text('4.8', style:TextStyle(
-                                    color:Colors.grey.shade300,
-                                    fontSize:14,
-                                    fontWeight: FontWeight.w400
-                                  ),),
-                                ],)
-                              ],
-                            )
-                          ],
-
-                        )
-                      ),
-                    ),
-                  )
-
-                ],
-               
-              
-              )
-
-            )
+            Destination(size: size, image: 'assets/images/2.jpeg',name1: 'Mount Narangala',name2: ', Sri Lanka',name3: 'Badulla, Sri Lanka',rating: '4.8',)
 
 
            
@@ -323,4 +231,123 @@ class _HomeScreenState extends State<HomeScreen> {
 
     
   
+}
+
+class Destination extends StatelessWidget {
+  String image;
+  String name1;
+  String name2;
+  String name3;
+  String rating;
+
+   Destination({
+    super.key,
+    required this.size,
+    required this.image,
+    required this.name1,
+    required this.name2,
+    required this.name3,
+    required this.rating
+
+
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width* 0.7,
+      height: (size.width* 0.7)*1.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover
+        )
+    
+      ),
+    
+      child: Stack(
+        children: [
+          Positioned(
+            top: 12,right:8,
+            child: CircleAvatar(
+              radius: 20,
+              child:Icon(Icons.favorite_border, color:Colors.grey.shade200,),
+              backgroundColor: Colors.black.withAlpha(30),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom:8.0),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                width: size.width* 0.6,
+                height: 60,
+                decoration: BoxDecoration(
+                  
+                  color: Colors.grey.shade900.withAlpha(90),
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child:Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(name1, style:TextStyle(
+                          color:Colors.white,
+                          fontSize:16,
+                          fontWeight: FontWeight.w800
+                        ),),
+                        Text(name2, style:TextStyle(
+                          color:Colors.grey.shade100,
+                          fontSize:13,
+                          fontWeight: FontWeight.w400
+                        ),),  
+                      ],
+    
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.location_on_rounded, color: Colors.grey.shade300,size:20),
+                            SizedBox(width:4,),
+                            Text(name3, style:TextStyle(
+                              color:Colors.grey.shade300,
+                              fontSize:14,
+                              fontWeight: FontWeight.w400
+                            ),),
+                        
+                            
+                        
+                          ],
+                        ),
+                        Row(children: [
+                          Icon(Icons.star_border, color: Colors.amber.shade400,size: 20),
+                          Text(rating, style:TextStyle(
+                            color:Colors.grey.shade300,
+                            fontSize:14,
+                            fontWeight: FontWeight.w400
+                          ),),
+                        ],)
+                      ],
+                    )
+                  ],
+    
+                )
+              ),
+            ),
+          )
+    
+        ],
+       
+      
+      )
+    
+    );
+  }
 }
